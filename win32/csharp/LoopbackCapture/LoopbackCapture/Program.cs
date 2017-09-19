502ec2b7d58033d8548167449421ea9c617f7341
Tony Su <peitaosu@163.com>
2017-09-19 18:02:15 +0800

    capture loopback and save as specific format: PCM

--- win32/csharp/LoopbackCapture/LoopbackCapture/Program.cs
+++ win32/csharp/LoopbackCapture/LoopbackCapture/Program.cs
@@ -1,11 +1,13 @@
 ﻿/* Some Code Fragments from:
    1. https://stackoverflow.com/questions/18812224/c-sharp-recording-audio-from-soundcard - Florian R.
+   2. https://github.com/filoe/cscore/blob/master/Samples/RecordWithSpecificFormat/Program.cs - CSCore Source Code
  */
 
 using System;
 using CSCore;
 using CSCore.SoundIn;
 using CSCore.Codecs.WAV;
+using CSCore.Streams;
 using System.Threading;
 
 namespace LoopbackCapture
@@ -46,40 +48,65 @@ namespace LoopbackCapture
                     break;
             }
 
-            using (WasapiCapture capture = new WasapiLoopbackCapture())
+            int sampleRate = 48000;
+            int bitsPerSample = 24;
+
+            //create a new soundIn instance
+            using (WasapiCapture soundIn = new WasapiLoopbackCapture())
             {
 
-                //initialize the selected device for recording
-                capture.Initialize();
+                //initialize the soundIn instance
+                soundIn.Initialize();
 
-                //set WaveFormatTag to Pcm
-                WaveFormat wave_format = new WaveFormat(capture.WaveFormat.SampleRate, capture.WaveFormat.BitsPerSample, capture.WaveFormat.Channels, AudioEncoding.Pcm);
+                //create a SoundSource around the the soundIn instance
+                SoundInSource soundInSource = new SoundInSource(soundIn) { FillWithZeros = false };
 
-                //create a wavewriter to write the data to
-                using (WaveWriter w = new WaveWriter(output_file, wave_format))
+                //create a source, that converts the data provided by the soundInSource to any other format
+
+                IWaveSource convertedSource = soundInSource
+                    .ChangeSampleRate(sampleRate) // sample rate
+                    .ToSampleSource()
+                    .ToWaveSource(bitsPerSample); //bits per sample
+
+                //channels...
+                using (convertedSource = convertedSource.ToStereo())
                 {
-                    //setup an eventhandler to receive the recorded data
-                    capture.DataAvailable += (s, e) =>
-                    {
-                        //save the recorded audio
-                        w.Write(e.Data, e.Offset, e.ByteCount);
-                    };
 
-                    //start recording
-                    capture.Start();
-
-                    //delay and keep recording
-                    if (time != 0)
+                    //create a new wavefile
+                    using (WaveWriter waveWriter = new WaveWriter(output_file, convertedSource.WaveFormat))
                     {
-                        Thread.Sleep(time);
+
+                        //register an event handler for the DataAvailable event of the soundInSource
+                        soundInSource.DataAvailable += (s, e) =>
+                        {
+                            //read data from the converedSource
+                            byte[] buffer = new byte[convertedSource.WaveFormat.BytesPerSecond / 2];
+                            int read;
+
+                            //keep reading as long as we still get some data
+                            while ((read = convertedSource.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                //write the read data to a file
+                                waveWriter.Write(buffer, 0, read);
+                            }
+                        };
+
+                        //start recording
+                        soundIn.Start();
+
+                        //delay and keep recording
+                        if (time != 0)
+                        {
+                            Thread.Sleep(time);
+                        }
+                        else
+                        {
+                            Console.ReadKey();
+                        }
+
+                        //stop recording
+                        soundIn.Stop();
                     }
-                    else
-                    {
-                        Console.ReadKey();
-                    }
-
-                    //stop recording
-                    capture.Stop();
                 }
             }
             return 0;
