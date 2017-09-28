6dadb16f0f0aba023a638afe80a9b21555ecaf72
Tony Su <peitaosu@163.com>
2017-09-28 19:49:18 +0800

    add LoopbackCapture.py for win32

---
+++ win32/LoopbackCapture.py
@@ -0,0 +1,19 @@
+import os, sys, subprocess
+
+def record_sounds(output_file="record.wav", time=0):
+    if "LOOPBACK_CAPTURE" not in os.environ:
+        print "Please set the %LOOPBACK_CAPTURE% before you start to capture."
+        sys.exit(-1)
+    if not os.path.isfile(os.environ["LOOPBACK_CAPTURE"]):
+        print "File Not Found. Please make sure the %LOOPBACK_CAPTURE% is correct."
+        sys.exit(-1)
+    Loopback_Capture_Path = os.environ["LOOPBACK_CAPTURE"]
+    if time is not 0:
+        process = subprocess.Popen("{} {} {}".format(
+            Loopback_Capture_Path, output_file, time), stdout=subprocess.PIPE)
+        exit_code = process.wait()
+    else:
+        process = subprocess.Popen("{} {}".format(
+            Loopback_Capture_Path, output_file), stdout=subprocess.PIPE)
+        exit_code = process.wait()
+    return exit_code
