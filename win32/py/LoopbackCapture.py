aa4ba96f522c056133c9fc1b4a6364dc605f5a53
Tony Su <peitaosu@163.com>
2018-09-01 18:17:44 +0800

    add python sample

---
+++ win32/py/LoopbackCapture.py
@@ -0,0 +1,39 @@
+import os, sys, clr
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+clr.AddReference("CSCore")
+from System import EventHandler, EventArgs, Array, Byte, Threading
+from CSCore import *
+from CSCore.SoundIn import *
+from CSCore.Codecs.WAV import *
+from CSCore.Streams import *
+
+output_file = "out.wav"
+time = 5000
+
+sampleRate = 48000
+bitsPerSample = 24
+
+soundIn = WasapiLoopbackCapture()
+soundIn.Initialize()
+soundInSource = SoundInSource(soundIn)
+soundInSource.FillWithZeros = False
+convertedSource = soundInSource
+convertedSource = FluentExtensions.ChangeSampleRate(convertedSource, sampleRate)
+waveWriter = WaveWriter(output_file, convertedSource.WaveFormat)
+#waveWriter = WaveWriter(output_file, soundInSource.WaveFormat)
+
+def capture(sender, event_args):
+    buffer = Array.CreateInstance(Byte, convertedSource.WaveFormat.BytesPerSecond / 2)
+    while True:
+        read = convertedSource.Read(buffer, 0, buffer.Length)
+        waveWriter.Write(buffer, 0, read)
+        if read <= 0:
+            break
+
+delegate = EventHandler(capture)
+soundInSource.DataAvailable += delegate
+
+soundIn.Start()
+Threading.Thread.Sleep(time)
+soundIn.Stop()
