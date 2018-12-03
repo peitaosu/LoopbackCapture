32336e83c05e6166ba3393e9f785dd16b50f02b8
Tony Su <peitaosu@163.com>
2018-12-03 23:43:29 +0800

    align csharp projects

---
+++ build/VersionAssembly.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LoopbackCapture")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Diplomatist Project")]
+[assembly: AssemblyProduct("LoopbackCapture")]
+[assembly: AssemblyCopyright("Copyright © Tony Su 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
