e04afe70bc38eb1b5d5bdf0be3c8432cc6a5955a
Tony Su <peitaosu@163.com>
2018-12-10 22:41:29 +0800

    add build scripts

---
+++ build.csharp.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cd /d %~dp0
+
+rmdir /s /q bin
+rmdir /s /q obj
+
+msbuild win32\csharp\LoopbackCapture.csproj /p:Configuration=Debug /p:Platform=x64 /t:Clean,Build
+msbuild win32\csharp\LoopbackCapture.csproj /p:Configuration=Debug /p:Platform=x86 /t:Clean,Build
+msbuild win32\csharp\LoopbackCapture.csproj /p:Configuration=Release /p:Platform=x64 /t:Clean,Build
+msbuild win32\csharp\LoopbackCapture.csproj /p:Configuration=Release /p:Platform=x86 /t:Clean,Build
